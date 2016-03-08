4a4eac5a84b73ff15989e05c3a32fef2b58d2509
Ragnar Dahlén <r.dahlen@gmail.com>
2016-03-08 14:22:25 +0000

    argc whoopsie

--- oomg.c
+++ oomg.c
@@ -4,11 +4,11 @@
  
 int main(int argc, char *argv[]) {
   int szalloc = 65536, nallocs = INT_MAX;
-  if (argc >= 1) {
-    szalloc = atoi(argv[0]);
+  if (argc >= 2) {
+    szalloc = atoi(argv[1]);
   }
-  if (argc == 2) {
-    nallocs = atoi(argv[1]);
+  if (argc == 3) {
+    nallocs = atoi(argv[2]);
   }  
   
   for (int i = 0; i < nallocs; i++) {
